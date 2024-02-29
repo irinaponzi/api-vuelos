7f031882ecb0a46ae21b80145bd2f85f5b9109a0
irinaponzi <irina.ponzi@gmail.com>
2024-02-29 00:47:43 -0300

    servicio y controller modificado. practica miercoles 28-02

--- src/main/java/codoacodo/vuelosapi/controller/VueloController.java
+++ src/main/java/codoacodo/vuelosapi/controller/VueloController.java
@@ -3,10 +3,9 @@ package codoacodo.vuelosapi.controller;
 import codoacodo.vuelosapi.model.Vuelo;
 import codoacodo.vuelosapi.service.VueloService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
@@ -16,8 +15,39 @@ public class VueloController {
     @Autowired
     private VueloService vueloService;
 
-    @GetMapping
+    @GetMapping("/obtenerTodos")
     public List<Vuelo> getVuelos() {
         return vueloService.listarVuelos();
     }
+
+    @GetMapping("/obtenerVuelo/{id}")
+    public Vuelo getVueloPorId(@PathVariable Long id) {
+        return vueloService.vueloPorId(id);
+    }
+
+    @PostMapping("/crearVuelo")
+    public String crearVuelo(@RequestBody Vuelo vuelo) {
+        return vueloService.crearVuelo(vuelo);
+    }
+
+    @PutMapping("/modificarVuelo/{id}")
+    public String modificarVuelo(@PathVariable Long id, @RequestBody Vuelo vuelo) {
+        return vueloService.modificarVuelo(id, vuelo);
+    }
+
+    @PatchMapping("/modificarPrecioVuelo/{id}")
+    public String modificarPrecioVuelo(@PathVariable Long id, @RequestParam double precio) {
+        return vueloService.modificarPrecioVuelo(id, precio);
+    }
+
+    @PatchMapping("/modificarFechaVuelo/{id}")
+    public String modificarFechaVuelo(@PathVariable Long id, @RequestParam LocalDateTime fechaHoraSalida, @RequestParam LocalDateTime fechaHoraLlegada) {
+        return vueloService.modificarFechaVuelo(id, fechaHoraSalida, fechaHoraLlegada);
+    }
+
+    @DeleteMapping("/eliminarVuelo/{id}")
+    public String eliminarVueloPorId(@PathVariable Long id) {
+        return vueloService.eliminarVueloPorId(id);
+    }
+
 }
--- src/main/java/codoacodo/vuelosapi/service/VueloService.java
+++ src/main/java/codoacodo/vuelosapi/service/VueloService.java
@@ -3,6 +3,7 @@ package codoacodo.vuelosapi.service;
 import codoacodo.vuelosapi.model.Vuelo;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static codoacodo.vuelosapi.Utils.listaDeVuelos;
@@ -10,7 +11,68 @@ import static codoacodo.vuelosapi.Utils.listaDeVuelos;
 @Service
 public class VueloService {
 
+    private List<Vuelo> vuelos = listaDeVuelos();
+
     public List<Vuelo> listarVuelos() {
-        return listaDeVuelos();
+        return vuelos;
+    }
+
+    public Vuelo vueloPorId(Long id) {
+
+        return vuelos.stream()
+                .filter(vuelo -> vuelo.getId().equals(id))
+                .findFirst()
+                .orElse(null);
+    }
+
+    public String crearVuelo(Vuelo vuelo) {
+        vuelos.add(vuelo);
+        return "El vuelo se ha creado exitosamente";
+    }
+
+    public String modificarVuelo(Long id, Vuelo vuelo) {
+
+        Vuelo vueloModificar = vueloPorId(id);
+        if(vueloModificar != null) {
+            vuelos.remove(vueloModificar);
+            vuelos.add(vuelo);
+            return "El vuelo " + id + " ha sido moficado";
+        }
+        else
+            return "El vuelo " + id + " no ha sido encontrado";
+    }
+
+    public String modificarPrecioVuelo(Long id, double precio) {
+
+        Vuelo vueloModificar = vueloPorId(id);
+        if(vueloModificar != null) {
+            vueloModificar.setPrecio(precio);
+            return "El precio del vuelo " + id + " ha sido moficado";
+        }
+        else
+            return "El vuelo " + id + " no ha sido encontrado";
+    }
+
+    public String modificarFechaVuelo(Long id, LocalDateTime fechaHoraSalida, LocalDateTime fechaHoraLlegada) {
+
+        Vuelo vueloModificar = vueloPorId(id);
+        if(vueloModificar != null) {
+            vueloModificar.setFechaHoraSalida(fechaHoraSalida);
+            vueloModificar.setFechaHoraLlegada(fechaHoraLlegada);
+            return "La fecha del vuelo " + id + " ha sido moficada";
+        }
+        else
+            return "El vuelo " + id + " no ha sido encontrado";
+    }
+
+    public String eliminarVueloPorId(Long id) {
+
+        Vuelo vueloEliminar = vueloPorId(id);
+        if(vueloEliminar != null) {
+            vuelos.remove(vueloEliminar);
+            return "El vuelo " + id + " ha sido eliminado";
+        }
+        else
+            return "El vuelo " + id + " no ha sido encontrado";
     }
 }
